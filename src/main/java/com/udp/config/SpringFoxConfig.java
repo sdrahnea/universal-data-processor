936907ca36463ab7147d9ea7b7c25c0e8c8362e2
Sergiu Drahnea <s.drahnea@gmail.com>
2022-01-07 09:02:17 +0200

    add swagger config

---
+++ src/main/java/com/udp/config/SpringFoxConfig.java
@@ -0,0 +1,35 @@
+package com.udp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SpringFoxConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "File Management System API",
+                "Manage file contents (upload/download/storage).",
+                "1.0.2",
+                "Terms of service",
+                null,
+                "License of API", "API license URL", Collections.emptyList());
+    }
+
+}
